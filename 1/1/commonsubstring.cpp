a850dc5e250a3dc7e5c8e2fb2967e8d48baec08e
LiyangTseng <38553834+LiyangTseng@users.noreply.github.com>
2019-12-24 00:20:07 +0800

    floor 18 finished

--- 1/1/commonsubstring.cpp
+++ 1/1/commonsubstring.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 CommonSubstring::CommonSubstring()
 {
@@ -19,24 +20,40 @@ string CommonSubstring::solve(string s)
     remove_copy_if(s2.begin(),s2.end(),back_inserter(newS2),ptr_fun<int, int>(&ispunct));
     newS1 = capitailize(newS1);
     newS2 = capitailize(newS2);
-    cout << newS1 << endl << newS2 << endl;
-    for (int i=0; i<newS1.length(); i++)
+
+    //X = newS1, Y = newS2, m = newS1.length(), n = newS2.length()
+    int maxlen = 0;			// stores the max length of LCS
+    int endingIndex = newS1.length();	// stores the ending index of LCS in X
+
+    // lookup[i][j] stores the length of LCS of substring
+    // X[0..i-1], Y[0..j-1]
+    int lookup[newS1.length() + 1][newS2.length() + 1];
+
+    // initialize all cells of lookup table to 0
+    memset(lookup, 0, sizeof(lookup));
+
+    // fill the lookup table in bottom-up manner
+    for (int i = 1; i <= newS1.length(); i++)
     {
-        string match = "";
-        for (int j=0; j<newS2.length(); j++)
+        for (int j = 1; j <= newS2.length(); j++)
         {
-            if (i + j < newS1.length() && newS1[i + j] == newS2[j])
-                match += newS2[j];
-            else
+            // if current character of newS1 and newS2 matches
+            if (newS1[i - 1] == newS2[j - 1])
             {
-                if (match.length() > result.length())
-                    result = match;
-                match = "";
+                lookup[i][j] = lookup[i - 1][j - 1] + 1;
+
+                // update the maximum length and ending index
+                if (lookup[i][j] > maxlen)
+                {
+                    maxlen = lookup[i][j];
+                    endingIndex = i;
+                }
             }
         }
     }
 
-    return result;
+    // return Longest common substring having length maxlen
+    return newS1.substr(endingIndex - maxlen, maxlen);
 }
 
 string CommonSubstring::capitailize(string s)
