92c44885cef10115c07a5d849bef4a051f7c1377
fumchin <fumchin@gmail.com>
2020-01-14 15:20:08 +0800

    floor5 fixed (without delay)

--- 1/1/fib.cpp
+++ 1/1/fib.cpp
@@ -27,13 +27,44 @@ int Fib::shift(long long int n){
     for(int i=2;i<=n;i++){
         result = f0+f1;
         if(result>=60){
-            result%=60;
+            result-=60;
         }
-        f0 = f1;
+        //result = ((f0%60)+(f1%60))%60;
+        if(f1>=60) f0 = f1 - 60;
+        else f0 = f1;
         f1 = result;
     }
     return  result;
 }
+
+
+long long Fib::get_pisano_period(long long m) {
+    long long a = 0, b = 1, c = a + b;
+    for (int i = 0; i < m * m; i++) {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+        if (a == 0 && b == 1) return i + 1;
+    }
+}
+
+long long Fib::get_fibonacci_huge(long long n, long long m) {
+    long long remainder = n % get_pisano_period(m);
+
+    long long first = 0;
+    long long second = 1;
+
+    long long res = remainder;
+
+    for (int i = 1; i < remainder; i++) {
+        res = (first + second) % m;
+        first = second;
+        second = res;
+    }
+
+    return res % m;
+}
+
 
 string Fib::solve(string s){
     stringstream ss;
@@ -47,13 +78,11 @@ string Fib::solve(string s){
     long long int temp;   //temp = fib(n)%60
     //long long int temp[2]={0};
     while(ss>>n){
-//        cout<<n<<endl;
-     //   fast_fib(n,ans);
-        temp = shift(n);
+        temp = get_fibonacci_huge(n,60);
         ans = find_last_digit(f,temp);
         result.append(to_string(ans)+' ');
     }
     result.erase(result.length()-1);
-
+    cout<<result<<endl;
     return result;
 }
